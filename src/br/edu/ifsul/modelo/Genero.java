808d89e6ec435d2c3c47baed47cd14d645a96530
V_M_FT <V_M_FT@DESKTOP-3TKRK7T>
2018-06-08 22:41:04 -0300

    Inicio- model

---
+++ src/br/edu/ifsul/modelo/Genero.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.modelo;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ *
+ * @author V_M_FT
+ */
+@Entity
+@Table(name = "genero")
+public class Genero implements Serializable {
+
+    @Id
+    @SequenceGenerator(name = "seq_genero", sequenceName = "seq_genero_id",allocationSize = 1)
+    @GeneratedValue(generator = "seq_genero", strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @NotNull(message = "O nome não pode ser nulo")
+    @NotBlank(message = "O nome não pode ser em branco")
+    @Length(max = 40, message = "O nome não pode ter mais que {max} caracteres")
+    @Column(name = "nome", length = 40, nullable = false)
+    private String nome;
+    
+    @NotNull(message = "A Descrição não pode ser nulo")
+    @NotBlank(message = "A Descrição não pode ser em branco")
+    @Length(max = 40, message = "A Descrição não pode ter mais que {max} caracteres")
+    @Column(name = "descricao", length = 40, nullable = false)
+    private String descricao;
+
+    
+    public Genero() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Genero other = (Genero) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
